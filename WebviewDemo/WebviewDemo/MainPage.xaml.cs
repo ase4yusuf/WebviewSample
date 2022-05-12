dfd9a28b05e3dc1011e6c5658b942b8e0f8e50ea
yusuf.asemokhai <yusuf.asemokhai@allianz.com>
2022-05-12 14:19:10 +0100

    Second Commit

---
+++ WebviewDemo/WebviewDemo/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace WebviewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.Source = "https://webapp.staging.carscan.ai/2417/ClientTEST123/081056788/RJ47A912/chander@carscan.co.za/%3F_id%3D626118bec080a1118923b2da";
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            RunTimePermission();
+        }
+
+        public async void RunTimePermission()
+        {
+            var status = PermissionStatus.Unknown;
+
+
+            status = await CrossPermissions.Current.RequestPermissionAsync<CameraPermission>();
+
+            if (status != PermissionStatus.Granted)
+            {
+
+                status = await Utils.CheckPermissions(Permission.Camera);
+            }
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //var url = "https://" + UrlEntry.Text;
+            //Browser.Source = url;
+        }
+
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            RunTimePermission();
+        }
+
+
+
+    }
+}
---
+++ WebviewDemo/WebviewDemo/Utils.cs
@@ -0,0 +1,77 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebviewDemo
+{
+    public static class Utils
+    {
+        public static async Task<PermissionStatus> CheckPermissions(Permission permission)
+        {
+            var permissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync<CameraPermission>();
+            bool request = false;
+            if (permissionStatus == PermissionStatus.Denied)
+            {
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+
+                    var title = $"{permission} Permission";
+                    var question = $"To use this plugin the {permission} permission is required. Please go into Settings and turn on {permission} for the app.";
+                    var positive = "Settings";
+                    var negative = "Maybe Later";
+                    var task = Application.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
+                    if (task == null)
+                        return permissionStatus;
+
+                    var result = await task;
+                    if (result)
+                    {
+                        CrossPermissions.Current.OpenAppSettings();
+                    }
+
+                    return permissionStatus;
+                }
+
+                request = true;
+
+            }
+
+            if (permissionStatus != PermissionStatus.Granted)
+            {
+                var newStatus = await CrossPermissions.Current.CheckPermissionStatusAsync<CameraPermission>();
+
+                if (newStatus == PermissionStatus.Granted)
+                {
+                    return permissionStatus;
+                }
+
+                permissionStatus = newStatus;
+
+                if (newStatus != PermissionStatus.Granted)
+                {
+                    permissionStatus = newStatus;
+                    var title = $"{permission} Permission";
+                    var question = $"To use the plugin the {permission} permission is required.";
+                    var positive = "Settings";
+                    var negative = "Maybe Later";
+                    var task = App.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
+                    if (task == null)
+                        return permissionStatus;
+
+                    var result = await task;
+                    if (result)
+                    {
+                        CrossPermissions.Current.OpenAppSettings();
+                    }
+                    return permissionStatus;
+                }
+            }
+
+            return permissionStatus;
+        }
+    }
+}
